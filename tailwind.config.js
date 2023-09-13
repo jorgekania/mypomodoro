450733a9b6116fc43d1cccace90dbf178a0b3171
Jorge Kania <jorgefkania@gmail.com>
2023-09-13 08:12:11 -0300

    Testes de layout tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
         "color-primary": "#01051e",
         "color-primary-light": "#020726",
         "color-primary-dark": "#010417",
-        "color-secondary": "#00BF63",
+        "color-secondary": "#79111c",
         "color-gray": "#333",
         "color-white": "#fff",
         "color-blob": "#A427DF",
